03bf2c16139f60fe135afcc380c42c74d5b1a881
Iftikhar uddin <iftipro@gmail.com>
2023-10-16 23:10:06 +0500

    Get and POST method testing

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send({
-        message: "My data",
-        id:"uniqueId",
-        description: "D ta na D"
-    }); // 127.0.0.1:3000
+app.use(express.json({ extended: false }));
+
+const nfts = JSON.parse(
+    fs.readFileSync(`${__dirname}/nft-data/data/nft-simple.json`)
+);
+
+app.get('/test', (req, res) => {
+    res.json({msg: 'Hello world!!!'});
+});
+
+// get request
+app.get("/api/v1/nfts", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            nfts
+        }
+    });
+});
+
+// post request
+app.post("/api/v1/nfts", (req, res) => {
+    // console.log(req.body);
+    // res.send("Post my NFT");
+    const newId = nfts[nfts.length -1].id + 1;
+    const newNFTs = Object.assign({id: newId}, req.body);
+
+    nfts.push(newNFTs);
+
+    fs.writeFileSync(`${__dirname}/nft-data/data/nft-simple.json`, JSON.stringify(nfts), err => {
+        res.status(201).json({
+            status: "success",
+            nft: newNFTs
+        })
+    });
+
+    // res.send("Post my NFT");
 });
 
 const port = 3000;
