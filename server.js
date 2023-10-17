2b18ff02b883a7fb3b47a2af3c57b07083319722
Iftikhar uddin <iftipro@gmail.com>
2023-10-17 23:39:54 +0500

    First data inserted to MongoDB from our app

--- server.js
+++ server.js
@@ -1,8 +1,51 @@
 const app = require("./App");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env"});
-console.log(process.env.NODE_ENV);
+
+const DB = process.env.DATABASE_URL;
+
+mongoose.connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+}).then((con)=>{
+    // console.log(con.connection);
+    console.log("Connected Successfully");
+});
+
+// console.log(process.env.NODE_ENV);
+
+const nftSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 3
+    },
+    price: {
+        type: Number,
+        required: [true, "NFT must have a price"]
+    },
+});
+
+const NFT = mongoose.model("NFT", nftSchema);
+
+const testNFT = new NFT({
+    name: "My wild NFT",
+    rating: 1,
+    price: 12,
+});
+
+testNFT.save().then(docNFT => {
+    console.log(docNFT);
+}).catch(error => {
+    console.log("Error", error);
+});
 
 const port = process.env.PORT || 3000;
 
