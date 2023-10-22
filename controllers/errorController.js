b8bf0b5d26ed20d4631e145fddc579c13d69508a
Iftikhar uddin <iftipro@gmail.com>
2023-10-22 23:56:09 +0500

    Dev and production errors logic added

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
-module.exports = (err, req, res, next)=> {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
+// for development
+const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
+        error: err,
+        stack: err.stack
     });
+};
+
+// for Production
+const sendErrorPro = (err, res) => {
+    if(err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        res.status(500).json({
+            status: "error",
+            message: "Something went very wrong"
+        })
+    }
+
+};
+
+module.exports = (err, req, res, next)=> {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    if(process.env.NODE_ENV === "development") {
+        sendErrorDev(err, res);
+    } else if(process.env.NODE_ENV === "production") {
+        sendErrorPro(err, res);
+    }
     next();
-}
+};
