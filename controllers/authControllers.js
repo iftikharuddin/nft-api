4ff5a3a4caa013ca92c89280bf9965bb82414afd
Iftikhar uddin <iftipro@gmail.com>
2023-10-28 23:09:20 +0500

    JWT implemented

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require("./../models/userModel");
 const catchAsync = require("./../Utils/catchAsync");
 
@@ -11,8 +12,13 @@ exports.signup = catchAsync( async (req, res, next ) => {
         passwordConfirm: req.body.passwordConfirm
     });
 
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+
     res.status(200).json({
         status: "Success",
+        token,
         data: {
             user: newUser
         }
