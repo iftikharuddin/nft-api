714015ecaadde73f4e61ea095bf9ca798d109618
Iftikhar uddin <iftipro@gmail.com>
2023-10-31 19:52:40 +0500

    Update me function added for users

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,14 +3,34 @@ const APIFeatures = require("./../Utils/apiFeatures");
 const catchAsync = require("./../Utils/catchAsync");
 const AppError = require("./../Utils/appError");
 
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el=>{
+        if(allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
     // create error if updating password .. this is only for data updating
     if(req.body.password || req.body.passwordConfirm) {
         return next(new AppError("This route is not for password update", 400));
     }
     // update user data
+    const filteredBody = filterObj(req.body, "name", "email");
+
+    const updateUser = await User.findByIdAndUpdate(
+        req.user.id, filteredBody, {
+            new: true,
+            runValidators: true
+        }
+    );
+
     res.status(200).json({
-        status: "success"
+        status: "success",
+        data: {
+            user: updateUser
+        }
     })
 
 });
